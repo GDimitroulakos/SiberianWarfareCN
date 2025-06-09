d8a0186a2a8f5a58462ceb502b8b41339d3278cd
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2025-06-09 12:43:00 +0300

    Add INodeMetadataPrinter interface and base printer class
    Introduces the `INodeMetadataPrinter` interface and the `BaseNodeMetadataGraphvizPrinter` class that implements it. The base class requires a parent printer in its constructor, throwing an `ArgumentNullException` if null. The `ToString` method delegates to the parent printer, with a fallback to throw a `NotImplementedException` if no parent is set. A nested class `DFSDirectedMetadataGraphVizPrinter` is also defined but not yet implemented.

---
+++ CommunicationNetwork/Graph/NodeMetadataGraphvizPrinters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationNetwork.Graph {
+
+    
+    public interface INodeMetadataPrinter {
+        string ToString(INode node);
+    }
+
+    public class BaseNodeMetadataGraphvizPrinter : INodeMetadataPrinter{
+    
+        INodeMetadataPrinter _parent;
+
+        public BaseNodeMetadataGraphvizPrinter(INodeMetadataPrinter parent) {
+            _parent = parent ?? throw new ArgumentNullException(nameof(parent), "Parent printer cannot be null.");
+        }
+
+        public string ToString(INode node) {
+            if (_parent != null) {
+                return _parent.ToString(node);
+            }
+            else {
+                throw new NotImplementedException("No parent printer set.");
+            }
+        }
+
+        public class DFSDirectedMetadataGraphVizPrinter : BaseNodeMetadataGraphvizPrinter{
+
+
+
+        }
+    }
+    
+
+}
