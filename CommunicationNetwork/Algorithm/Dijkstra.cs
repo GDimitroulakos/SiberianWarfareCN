bfb370864e93daac2546e6e5dda03f82dbb3ed1a
Grigoris Dimitroulakos <dhmhgre@gmail.com>
2025-06-13 09:22:51 +0300

    Add Dijkstra algorithm and skeleton for Bellman-Ford and Floyd-Warshall
    - Implemented the Dijkstra class inheriting from BaseAlgorithm with initial structure.
    - Added private field for graph representation and static metadata property.
    - Created BellmanFord and FloydWarshall classes with basic structure and necessary using directives.

--- CommunicationNetwork/Algorithm/Dijkstra.cs
+++ CommunicationNetwork/Algorithm/Dijkstra.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunicationNetwork.Graph;
 
 namespace CommunicationNetwork.Algorithm {
     public class Dijkstra : BaseAlgorithm {
+
+        IGraph _graph;
+        public static string MetadataKey => "Dijkstra";
+
+        public override void Execute() {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize() {
+            throw new NotImplementedException();
+        }
     }
 }
